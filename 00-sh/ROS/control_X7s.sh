8810b2eac6fea6857c7f533e560bcb0f91d377b3
josh <1071537404@qq.com>
2025-07-11 15:20:53 +0800

    更新脚本

--- 00-sh/ROS/control_X7s.sh
+++ 00-sh/ROS/control_X7s.sh
@@ -4,9 +4,9 @@ workspace=$(pwd)
 source ~/.bashrc
 
 # CAN
-gnome-terminal -t "can1" -x sudo bash -c "cd ${workspace}; cd ../..; cd ARX_CAN/arx_can; ./arx_can1.sh; exec bash;"
+gnome-terminal -t "can1" -x sudo bash -c "cd ${workspace}; cd ../..; cd ARX_CAN/arx_can; ./arx_can0.sh; exec bash;"
 sleep 0.1
-gnome-terminal -t "can3" -x sudo bash -c "cd ${workspace}; cd ../..; cd ARX_CAN/arx_can; ./arx_can3.sh; exec bash;"
+gnome-terminal -t "can3" -x sudo bash -c "cd ${workspace}; cd ../..; cd ARX_CAN/arx_can; ./arx_can1.sh; exec bash;"
 sleep 0.1
 gnome-terminal -t "can5" -x sudo bash -c "cd ${workspace}; cd ../..; cd ARX_CAN/arx_can; ./arx_can5.sh; exec bash;"
 sleep 1
